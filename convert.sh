4d542ce2436c462aac2d52c94c4237545d25cf5f
atsushieno <atsushieno@gmail.com>
2022-09-06 23:48:09 +0800

    add more drafts.

--- convert.sh
+++ convert.sh
@@ -4,6 +4,8 @@ for f in md/images/*.svg ; do
   node node_modules/svgexport/bin/index.js $f articles/images/$(basename $f .svg).png; 
 done
 
+cp md/images/*.png articles/images/
+
 for i in md/*.md ; do 
   md2review $i > articles/$(basename -s .md $i).re ;
 done
